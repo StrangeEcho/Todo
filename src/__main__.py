d915ba3567ad4a4d0864a3a9f00f44e654ea71fd
Yato <tylergoodmen312@gmail.com>
2023-10-24 15:40:39 -0400

    Task Modal Implementation

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,13 @@
 import asyncio
-
+import os
 from core.bot import TodoBot
 
+bot = TodoBot()
+
 if __name__ == "__main__":
-    asyncio.run(TodoBot().start())
+    if os.name == "nt":
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+    try:
+        asyncio.run(bot.start())
+    except KeyboardInterrupt:
+        pass
--- src/cogs/meta.py
+++ src/cogs/meta.py
@@ -1,9 +1,52 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-
+from discord.ui import Modal, TextInput
 from core.bot import TodoBot
+from typing import Literal
 
+class TaskModal(Modal):
+    def __init__(self, channel: int, *, modal_typle: Literal["Map", "Scripts"]):
+        super().__init__(
+            title="Map Task" if modal_typle == "Map" else "Scripting Task",
+        )
+        self.task_title = TextInput(
+            label="Task Title",
+            placeholder="The title of the task that you are proposing",
+            style=discord.TextStyle.long
+        )
+        self.task_description = TextInput(
+            label="Task Description",
+            placeholder="Describe the tasks taht you want to propose",
+            style=discord.TextStyle.long
+        )
+        self._todo_channel = channel 
+        self._reactions = {"👷", "✅", "❌"}
+        self.add_item(self.task_title)
+        self.add_item(self.task_description)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        chan = interaction.client.get_channel(self._todo_channel)
+        msg = await chan.send(
+            embed=discord.Embed(
+                title=self.task_title.value,
+                description=f"```\n{self.task_description.value}\n```",
+                color=discord.Color.red(),
+            )
+            .set_author(
+                name=f"Task proposed by: {interaction.user}",
+                icon_url=interaction.user.display_avatar.url
+            )
+            .set_footer(
+                text="❌ Decline | 👷 Claim | ✅ Mark Finished"
+            )
+        )
+        for reaction in self._reactions:
+            await msg.add_reaction(reaction)
+        await interaction.followup.send(f"Finished adding task to Todolist", ephemeral=True)
+        
+        
 
 class Meta(commands.Cog):
     def __init__(self, bot: TodoBot):
@@ -68,33 +111,14 @@ class Meta(commands.Cog):
     async def cleanup(self, ctx: commands.Context, limit: int = 15):
         """Cleanup the bots messages"""
         msgs = await ctx.channel.purge(limit=limit, check=lambda x: x.author.id == self.bot.user.id)
-        await ctx.send(f"Cleaned up {len(msgs)} of my messages")
+        await ctx.send(f"Cleaned up {len(msgs)} of my messages", delete_after=5)
 
     @app_commands.command()
-    @app_commands.describe(task="The task to be added to the todo list")
-    async def todo(self, inter: discord.Interaction, *, task: str):
+    @app_commands.describe(task_type="Pick between Map and Scripts")
+    async def todo(self, inter: discord.Interaction, task_type: Literal["Map", "Scripts"]):
         """Add a task to the todo list"""
-        await inter.response.defer()
-        channel = self.bot.get_channel(self.bot.todo_channel)
-        if not channel:
-            return await inter.followup.send("No TODO channel set")
-        msg = await channel.send(
-            embed=discord.Embed(
-                title="New Task!",
-                description=f"```\n{task}\n```",
-                color=discord.Color.red(),
-            )
-            .set_author(
-                name=f"Task proposed by: {inter.user}",
-            )
-            .set_thumbnail(url=inter.user.display_avatar.url)
-            .set_footer(
-                text="❌ Decline | 👷 Claim | ✅ Mark Finished"
-            )
-        )
-        for reaction in self.reactions:
-            await msg.add_reaction(reaction)
-        await inter.followup.send(f"Finished adding task to Todolist", ephemeral=True)
+        await inter.response.send_modal(TaskModal(self.bot.todo_channel, modal_typle=task_type))
+    
 
     @app_commands.command()
     @app_commands.describe(
